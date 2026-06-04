73326e247905d5f867608468700d9bcff8a2fec6
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-04 21:21:42 +0200

    index.js

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const {
   ActionRowBuilder,
   ButtonBuilder,
   ButtonStyle,
+  StringSelectMenuBuilder,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
 } = require("discord.js");
 const fs = require("fs");
 const path = require("path");
