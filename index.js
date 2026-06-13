6a1ea24942bbe8ec7d52c2791bac4cbd55103448
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-14 00:48:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const client = new Client({
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent,
         GatewayIntentBits.GuildMembers,
-       GatewayIntentBits.GuildPresences
+       GatewayIntentBits.GuildPresences,
         GatewayIntentBits.GuildVoiceStates
     ]
 });
