0cd192975267a7eb32831a75219269258ae13680
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-13 18:13:07 +0200

    Update pic.js

--- commands/utility/pic.js
+++ commands/utility/pic.js
@@ -1,1 +1,30 @@
-[]
+module.exports = {
+    name: "pic",
+
+    async run(message) {
+        const user = message.mentions.users.first() || message.author;
+
+        const comments = [
+            📸 ${user.username} est plutôt photogénique aujourd'hui.,
+            ✨ Cet avatar représente parfaitement ${user.username}.,
+            🎭 Voici le visage numérique de ${user.username}.,
+            🌟 Un avatar digne d'une légende.
+        ];
+
+        const comment = comments[Math.floor(Math.random() * comments.length)];
+
+        const embed = {
+            color: 0x5865F2,
+            title: 🖼️ Avatar de ${user.username},
+            description: comment,
+            image: {
+                url: user.displayAvatarURL({
+                    size: 1024,
+                    forceStatic: false
+                })
+            }
+        };
+
+        message.reply({ embeds: [embed] });
+    }
+};
