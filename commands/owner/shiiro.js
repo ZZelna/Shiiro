6243e9a69b2695e6a09780649a049ac1a76dcf40
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-14 00:38:06 +0200

    Update shiiro.js

--- commands/owner/shiiro.js
+++ commands/owner/shiiro.js
@@ -1,1 +1,42 @@
-[]
+const config = require("../../config.json");
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    name: "shiiro",
+
+    async run(message) {
+
+        if (!config.owner_ids.includes(message.author.id)) {
+            return message.reply("❌ Cette commande est réservée aux owners.");
+        }
+
+        const guild = message.guild;
+
+        const totalMembers = guild.memberCount;
+
+        const onlineMembers = guild.members.cache.filter(
+            m => m.presence?.status !== "offline"
+        ).size;
+
+        const vocalMembers = guild.members.cache.filter(
+            m => m.voice.channel
+        ).size;
+
+        const streamingMembers = guild.members.cache.filter(
+            m => m.voice.streaming
+        ).size;
+
+        const embed = new EmbedBuilder()
+    .setColor("#2B2D31")
+.setTitle(`${guild.name} 🏆 #🇫🇷 Statistiques !`)
+    .setDescription(
+👥 **Membres :** ${totalMembers.toLocaleString()} 🟢 **En ligne :** ${onlineMembers.toLocaleString()} 🎤 **En Vocal :** ${vocalMembers.toLocaleString()} 📺 **En stream :** ${streamingMembers.toLocaleString()} 🚀 **Boost :** ${guild.premiumSubscriptionCount.toLocaleString()}
+    )
+    .setThumbnail(guild.iconURL({ dynamic: true }))
+    .setTimestamp()
+
+        return message.channel.send({
+            embeds: [embed]
+        });
+    }
+};
