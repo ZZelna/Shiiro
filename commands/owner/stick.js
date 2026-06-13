7edeec255bb53c7e7bd1122f8a4a5a1af578c5d4
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-14 01:18:22 +0200

    Update stick.js

--- commands/owner/stick.js
+++ commands/owner/stick.js
@@ -1,1 +1,41 @@
-[]
+const fs = require("fs");
+const config = require("../../config.json");
+
+module.exports = {
+    name: "stick",
+
+    async run(message, args) {
+
+        if (!config.owner_ids.includes(message.author.id)) {
+            return message.reply("❌ Cette commande est réservée aux owners.");
+        }
+
+        const text = args.join(" ");
+
+        if (!text) {
+            return message.reply(
+                "❌ Utilisation : +stick <message>"
+            );
+        }
+
+        let stickies = {};
+
+        if (fs.existsSync("./data/stickies.json")) {
+            stickies = require("../../data/stickies.json");
+        }
+
+        stickies[message.channel.id] = {
+            content: text,
+            messageId: null
+        };
+
+        fs.writeFileSync(
+            "./data/stickies.json",
+            JSON.stringify(stickies, null, 2)
+        );
+
+        return message.reply(
+            "✅ Sticky configuré."
+        );
+    }
+};
