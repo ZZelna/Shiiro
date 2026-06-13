d79a5324dd825abd8efbe70c7c96f920b29f8321
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-14 00:08:01 +0200

    Update takerole.js

--- commands/owner/takerole.js
+++ commands/owner/takerole.js
@@ -1,1 +1,37 @@
+const config = require("../../config.json");
 
+module.exports = {
+    name: "takerole",
+
+    async run(message) {
+
+        if (!config.owner_ids.includes(message.author.id)) {
+            return message.reply("❌ Cette commande est réservée aux owners.");
+        }
+
+        const member = message.mentions.members.first();
+        const role = message.mentions.roles.first();
+
+        if (!member || !role) {
+            return message.reply(
+                "❌ Utilisation : +takerole @user @role"
+            );
+        }
+
+        try {
+
+            await member.roles.remove(role);
+
+            return message.reply(
+    `✅ Le rôle ${role} a été retiré à ${member.user.tag}.`
+);
+
+        } catch (err) {
+
+            return message.reply(
+                "❌ Impossible de retirer ce rôle."
+            );
+
+        }
+    }
+};
