3a6d9a7badcaa2858694e4834d513f1c7cec63c8
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-14 00:06:39 +0200

    Update addrole.js

--- commands/owner/addrole.js
+++ commands/owner/addrole.js
@@ -1,1 +1,37 @@
+const config = require("../../config.json");
 
+module.exports = {
+    name: "addrole",
+
+    async run(message) {
+
+        if (!config.owner_ids.includes(message.author.id)) {
+            return message.reply("❌ Cette commande est réservée aux owners.");
+        }
+
+        const member = message.mentions.members.first();
+        const role = message.mentions.roles.first();
+
+        if (!member || !role) {
+            return message.reply(
+                "❌ Utilisation : +addrole @user @role"
+            );
+        }
+
+        try {
+
+            await member.roles.add(role);
+
+            return message.reply(
+    `✅ Le rôle ${role} a été ajouté à ${member.user.tag}.`
+);
+
+        } catch (err) {
+
+            return message.reply(
+                "❌ Impossible d'ajouter ce rôle."
+            );
+
+        }
+    }
+};
