cd79c23e116d164050239aac760f4fd6859d83c5
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-13 18:45:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 require("dotenv").config();
 
 const fs = require("fs");
+
+client.commands = new Map();
+
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
 const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
@@ -23,7 +32,17 @@ for (const file of commandFiles) {
 }
 
 client.on("messageCreate", async (message) => {
+
     if (message.author.bot) return;
+
+    const args = message.content.slice(1).trim().split(/ +/);
+const commandName = args.shift()?.toLowerCase();
+
+const command = client.commands.get(commandName);
+
+if (command) {
+    return command.run(message, args);
+}
 
     if (message.content === "+help") {
     message.reply(`
