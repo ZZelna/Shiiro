dc2c03d8d3790f2d1acc0331773559b9a3aea510
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-14 00:27:03 +0200

    Update moderole add.js

--- commands/owner/moderole add.js
+++ commands/owner/moderole add.js
@@ -33,47 +33,9 @@ module.exports = {
             JSON.stringify(config, null, 2)
         );
 
-        return message.reply(
-            ✅ ${role.name} a été ajouté aux rôles modérateurs.
-        );
-    }
-};const fs = require("fs");
-const config = require("../../config.json");
-
-module.exports = {
-    name: "moderole",
-
-    async run(message, args) {
-
-        if (!config.owner_ids.includes(message.author.id)) {
-            return message.reply("❌ Cette commande est réservée aux owners.");
-        }
-
-        if (args[0] !== "add") return;
-
-        const role = message.mentions.roles.first();
-
-        if (!role) {
-            return message.reply(
-                "❌ Utilisation : +moderole add @role"
-            );
-        }
-
-        if (config.moderator_roles.includes(role.id)) {
-            return message.reply(
-                "❌ Ce rôle est déjà modérateur."
-            );
-        }
-
-        config.moderator_roles.push(role.id);
-
-        fs.writeFileSync(
-            "./config.json",
-            JSON.stringify(config, null, 2)
-        );
-
-        return message.reply(
+     return message.reply(
     `✅ ${role.name} a été ajouté aux rôles modérateurs.`
 );
     }
+
 };
