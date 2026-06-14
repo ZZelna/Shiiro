97355ba38d14cc9b0eb21410bf9da570686c476b
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-14 16:42:13 +0200

    Update remove.js

--- commands/tickets/remove.js
+++ commands/tickets/remove.js
@@ -1,1 +1,46 @@
-[]
+module.exports = {
+    name: "remove",
+
+    async run(message, args) {
+
+        if (!message.channel.name.startsWith("ticket-")) {
+            return message.reply(
+                "❌ Cette commande doit être utilisée dans un ticket."
+            );
+        }
+
+        if (
+            message.channel.topic &&
+            message.channel.topic !== message.author.id
+        ) {
+            return message.reply(
+                `❌ Ticket réclamé par <@${message.channel.topic}>`
+            );
+        }
+
+        const userId = args[0];
+
+        if (!userId) {
+            return message.reply(
+                "❌ Utilisation : +remove ID_DISCORD"
+            );
+        }
+
+        try {
+
+            await message.channel.permissionOverwrites.delete(
+                userId
+            );
+
+            return message.channel.send(
+                `❌ <@${userId}> a été retiré du ticket.`
+            );
+
+        } catch {
+
+            return message.reply(
+                "❌ ID invalide."
+            );
+        }
+    }
+};
