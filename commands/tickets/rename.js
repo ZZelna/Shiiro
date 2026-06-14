60482bca7beb2d00f485155d2aa55690da9ee187
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-14 13:29:13 +0000

    Update rename.js

--- commands/tickets/rename.js
+++ commands/tickets/rename.js
@@ -1,1 +1,26 @@
-[]
+module.exports = {
+    name: "rename",
+
+    async run(message, args) {
+
+        if (!message.channel.name.startsWith("ticket-")) {
+            return message.reply("❌ Cette commande doit être utilisée dans un ticket.");
+        }
+
+        const newName = args.join("-");
+
+        if (!newName) {
+            return message.reply(
+                "❌ Utilisation : +rename nouveau-nom"
+            );
+        }
+
+        await message.channel.setName(
+            `ticket-${newName}`
+        );
+
+        message.channel.send(
+            `✏️ Ticket renommé en \`${message.channel.name}\``
+        );
+    }
+}
