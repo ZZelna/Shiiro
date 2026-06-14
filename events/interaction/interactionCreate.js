15067ca562c514343952b836b8c50b6bad60debc
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-14 14:09:48 +0200

    Update interactionCreate.js

--- events/interaction/interactionCreate.js
+++ events/interaction/interactionCreate.js
@@ -1,1 +1,70 @@
-// Interaction event
+const {
+ChannelType,
+PermissionFlagsBits,
+EmbedBuilder
+} = require(“discord.js”);
+
+const ticketConfig = require(”../config/ticket”);
+
+module.exports = async (interaction) => {
+
+if (!interaction.isStringSelectMenu()) return;
+if (interaction.customId !== "ticket_create") return;
+const ticketType = interaction.values[0];
+const config = ticketConfig[ticketType];
+if (!config) return;
+const permissions = [
+    {
+        id: interaction.guild.id,
+        deny: [PermissionFlagsBits.ViewChannel]
+    },
+    {
+        id: interaction.user.id,
+        allow: [
+            PermissionFlagsBits.ViewChannel,
+            PermissionFlagsBits.SendMessages,
+            PermissionFlagsBits.ReadMessageHistory
+        ]
+    }
+];
+for (const roleId of config.roles) {
+    permissions.push({
+        id: roleId,
+        allow: [
+            PermissionFlagsBits.ViewChannel,
+            PermissionFlagsBits.SendMessages,
+            PermissionFlagsBits.ReadMessageHistory
+        ]
+    });
+}
+const channel = await interaction.guild.channels.create({
+    name: `ticket-${interaction.user.username}`,
+    type: ChannelType.GuildText,
+    parent: config.category,
+    permissionOverwrites: permissions
+});
+const roleMentions = config.roles
+    .map(id => `<@&${id}>`)
+    .join(" ");
+const embed = new EmbedBuilder()
+    .setColor("#2B2D31")
+    .setTitle("🎫 Ticket créé")
+    .setDescription(
+
+`Bonjour ${interaction.user},
+
+Merci de détailler votre demande.
+
+Un membre de l’équipe concernée va vous répondre prochainement.`
+);
+
+await channel.send({
+    content: roleMentions,
+    embeds: [embed]
+});
+await interaction.reply({
+    content: `✅ Votre ticket a été créé : ${channel}`,
+    ephemeral: true
+});
+
+};
