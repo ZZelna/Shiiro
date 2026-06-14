a9bd1c069db50a86a0dd0d610ff502e0349d74f8
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-14 16:53:44 +0000

    Update bypass.js

--- commands/moderation/bypass.js
+++ commands/moderation/bypass.js
@@ -1,1 +1,53 @@
-[]
+const fs = require("fs");
+const path = require("path");
+const bypassData = require("../../data/bypass.json");
+
+module.exports = {
+    name: "bypass",
+
+    async run(message, args) {
+
+        const allowedUsers = [
+            "1495920419528642631",
+            "1010620278541402226",
+            "1421806026537173032",
+            "1495834533797298176",
+            "1500273343054614529",
+            "1386994361216274472",
+            "1277800588578521146",
+            "1418370654251778168",
+            "1441136552842367027",
+            "1292848857704566866"
+        ];
+
+        if (!allowedUsers.includes(message.author.id)) {
+            return message.reply(
+                "❌ Vous n'avez pas la permission."
+            );
+        }
+
+        const user =
+            message.mentions.users.first() ||
+            await message.client.users.fetch(args[0]).catch(() => null);
+
+        if (!user) {
+            return message.reply(
+                "❌ Utilisation : +bypass @user"
+            );
+        }
+
+        if (!bypassData.users.includes(user.id)) {
+
+            bypassData.users.push(user.id);
+
+            fs.writeFileSync(
+                path.join(__dirname, "../../data/bypass.json"),
+                JSON.stringify(bypassData, null, 4)
+            );
+        }
+
+        return message.channel.send(
+            `✅ ${user} a été ajouté à la liste bypass.`
+        );
+    }
+};
