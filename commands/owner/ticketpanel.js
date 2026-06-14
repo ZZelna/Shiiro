c0f23c8ae7eb3f736e3b92c0976ea0e6a23b0b43
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-14 14:06:36 +0200

    Update ticketpanel.js

--- commands/owner/ticketpanel.js
+++ commands/owner/ticketpanel.js
@@ -1,1 +1,69 @@
+const config = require(”../../config.json”);
+const {
+EmbedBuilder,
+ActionRowBuilder,
+StringSelectMenuBuilder
+} = require(“discord.js”);
 
+module.exports = {
+name: “ticketpanel”,
+
+async run(message) {
+    if (!config.owner_ids.includes(message.author.id)) {
+        return message.reply("❌ Cette commande est réservée aux owners.");
+    }
+    const embed = new EmbedBuilder()
+        .setColor("#2B2D31")
+        .setTitle("🎫 Support Shiiro")
+        .setDescription(
+
+`Bienvenue sur le support de Shiiro.
+
+Veuillez sélectionner la catégorie correspondant à votre demande afin qu’un membre de l’équipe puisse vous aider.`
+);
+
+    const menu = new StringSelectMenuBuilder()
+        .setCustomId("ticket_create")
+        .setPlaceholder("Choisissez une catégorie")
+        .addOptions([
+            {
+                label: "Administration",
+                description: "Contacter l'administration",
+                value: "admin",
+                emoji: "👑"
+            },
+            {
+                label: "Gestion Abus",
+                description: "Signaler un abus",
+                value: "abus",
+                emoji: "⚠️"
+            },
+            {
+                label: "Gestion Staff",
+                description: "Contacter le staff",
+                value: "staff",
+                emoji: "📎"
+            },
+            {
+                label: "Gestion Casino",
+                description: "Support casino",
+                value: "casino",
+                emoji: "💰"
+            },
+            {
+                label: "Partenariat",
+                description: "Demande de partenariat",
+                value: "partenariat",
+                emoji: "🤝"
+            }
+        ]);
+    const row = new ActionRowBuilder()
+        .addComponents(menu);
+    await message.channel.send({
+        embeds: [embed],
+        components: [row]
+    });
+    return message.reply("✅ Panel ticket envoyé.");
+}
+
+};
