4e84a153fc9d0dfb6af684df4e52b7b0cb8674f9
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-14 15:56:08 +0000

    Update unjail.js

--- commands/moderation/unjail.js
+++ commands/moderation/unjail.js
@@ -1,1 +1,59 @@
-[]
+const fs = require("fs");
+const jailData = require("../data/jail.json");
+
+module.exports = {
+    name: "unjail",
+
+    async run(message, args) {
+
+        const allowedUsers = [
+            "1495920419528642631",
+            "1010620278541402226",
+            "1421806026537173032",
+            "1495834533797298176",
+            "1500273343054614529",
+            "1386994361216274472",
+            "1277800588578521146",
+            "1418370654251778168",
+            "1441136552842367027",
+            "1292848857704566866"
+        ];
+
+        if (!allowedUsers.includes(message.author.id)) {
+            return message.reply(
+                "❌ Vous n'avez pas la permission d'utiliser cette commande."
+            );
+        }
+
+        const member =
+            message.mentions.members.first() ||
+            message.guild.members.cache.get(args[0]);
+
+        if (!member) {
+            return message.reply(
+                "❌ Utilisation : +unjail @membre"
+            );
+        }
+
+        if (!jailData.users || !jailData.users[member.id]) {
+            return message.reply(
+                "❌ Ce membre n'est pas en prison."
+            );
+        }
+
+        const oldRoles = jailData.users[member.id];
+
+        await member.roles.set(oldRoles);
+
+        delete jailData.users[member.id];
+
+        fs.writeFileSync(
+            "./data/jail.json",
+            JSON.stringify(jailData, null, 4)
+        );
+
+        return message.channel.send(
+            `🔓 ${member} a été libéré par ${message.author}.`
+        );
+    }
+};
