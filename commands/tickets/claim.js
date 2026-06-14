d31af463397d973de3278e82124932bb86a2f101
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-14 16:16:54 +0200

    Update claim.js

--- commands/tickets/claim.js
+++ commands/tickets/claim.js
@@ -1,3 +1,5 @@
+const ticketConfig = require("../config/tickets");
+
 module.exports = {
     name: "claim",
 
@@ -15,8 +17,39 @@ module.exports = {
             );
         }
 
-        await message.channel.setTopic(
-            message.author.id
+        await message.channel.setTopic(message.author.id);
+
+        const creator = message.channel.permissionOverwrites.cache
+            .filter(overwrite =>
+                overwrite.allow.has("ViewChannel") &&
+                !overwrite.type === 0
+            );
+
+        const allRoles = [
+            "1506678765982318743",
+            "1509601528242110525",
+            "1506678694352261301",
+            "1506696551706267688",
+            "1506696757642530982",
+            "1506702398029172796",
+            "1506709088451690708"
+        ];
+
+        for (const roleId of allRoles) {
+
+            await message.channel.permissionOverwrites.edit(roleId, {
+                ViewChannel: false
+            });
+
+        }
+
+        await message.channel.permissionOverwrites.edit(
+            message.author.id,
+            {
+                ViewChannel: true,
+                SendMessages: true,
+                ReadMessageHistory: true
+            }
         );
 
         message.channel.send(
