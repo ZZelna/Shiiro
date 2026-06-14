eeebdb68f2cddf9d638a81b220b8eb3e0e1773f1
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-14 13:17:46 +0000

    Update claim.js

--- commands/tickets/claim.js
+++ commands/tickets/claim.js
@@ -1,1 +1,26 @@
-[]
+module.exports = {
+    name: "claim",
+
+    async run(message) {
+
+        if (!message.channel.name.startsWith("ticket-")) {
+            return message.reply(
+                "❌ Cette commande doit être utilisée dans un ticket."
+            );
+        }
+
+        if (message.channel.topic) {
+            return message.reply(
+                `❌ Ticket déjà réclamé par <@${message.channel.topic}>`
+            );
+        }
+
+        await message.channel.setTopic(
+            message.author.id
+        );
+
+        message.channel.send(
+            `📌 Ticket réclamé par ${message.author}`
+        );
+    }
+};
