5f832ac357eecaf0e59eeec4d9ed5b53dd676401
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-14 21:04:44 +0200

    Update lock.js

--- commands/owner/lock.js
+++ commands/owner/lock.js
@@ -1,1 +1,55 @@
+const {
+PermissionFlagsBits
+} = require("discord.js");
 
+const allowedUsers = [
+"1495920419528642631",
+"1010620278541402226",
+"1421806026537173032",
+"1495834533797298176",
+"1500273343054614529",
+"1386994361216274472",
+"1277800588578521146",
+"1418370654251778168"
+];
+
+module.exports = {
+name: "lock",
+
+async run(message) {
+    if (!allowedUsers.includes(message.author.id)) {
+        return;
+    }
+    const everyone =
+        message.guild.roles.everyone;
+    const perms =
+        message.channel.permissionOverwrites.cache.get(
+            everyone.id
+        );
+    const locked =
+        perms?.deny.has(
+            PermissionFlagsBits.SendMessages
+        );
+    if (locked) {
+        await message.channel.permissionOverwrites.edit(
+            everyone,
+            {
+                SendMessages: null
+            }
+        );
+        return message.channel.send(
+            "🔓 Salon déverrouillé."
+        );
+    }
+    await message.channel.permissionOverwrites.edit(
+        everyone,
+        {
+            SendMessages: false
+        }
+    );
+    return message.channel.send(
+        "🔒 Salon verrouillé."
+    );
+}
+
+};
