da68714ed4cd12d68ffed3672f4b54ee95b26eff
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-14 16:41:56 +0200

    Update add.js

--- commands/tickets/add.js
+++ commands/tickets/add.js
@@ -1,1 +1,51 @@
-[]
+module.exports = {
+    name: "add",
+
+    async run(message, args) {
+
+        if (!message.channel.name.startsWith("ticket-")) {
+            return message.reply(
+                "❌ Cette commande doit être utilisée dans un ticket."
+            );
+        }
+
+        if (
+            message.channel.topic &&
+            message.channel.topic !== message.author.id
+        ) {
+            return message.reply(
+                `❌ Ticket réclamé par <@${message.channel.topic}>`
+            );
+        }
+
+        const userId = args[0];
+
+        if (!userId) {
+            return message.reply(
+                "❌ Utilisation : +add ID_DISCORD"
+            );
+        }
+
+        try {
+
+            await message.channel.permissionOverwrites.edit(
+                userId,
+                {
+                    ViewChannel: true,
+                    SendMessages: true,
+                    ReadMessageHistory: true
+                }
+            );
+
+            return message.channel.send(
+                `✅ <@${userId}> a été ajouté au ticket.`
+            );
+
+        } catch {
+
+            return message.reply(
+                "❌ ID invalide."
+            );
+        }
+    }
+};
