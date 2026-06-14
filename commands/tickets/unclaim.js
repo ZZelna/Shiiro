0aa9b493eccbae9e10a63d032b37d6e75c9f6565
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-14 16:24:20 +0200

    Update unclaim.js

--- commands/tickets/unclaim.js
+++ commands/tickets/unclaim.js
@@ -5,35 +5,21 @@ module.exports = {
 
     async run(message) {
 
+        if (!message.channel.name.startsWith("ticket-")) {
+            return message.reply(
+                "❌ Cette commande doit être utilisée dans un ticket."
+            );
+        }
+
         if (!message.channel.topic) {
             return message.reply(
                 "❌ Ce ticket n'est pas réclamé."
             );
         }
 
-        const allRoles = [
-            "1506678765982318743",
-            "1509601528242110525",
-            "1506678694352261301",
-            "1506696551706267688",
-            "1506696757642530982",
-            "1506702398029172796",
-            "1506709088451690708"
-        ];
+        await message.channel.setTopic("");
 
-        for (const roleId of allRoles) {
-
-            await message.channel.permissionOverwrites.edit(roleId, {
-                ViewChannel: true,
-                SendMessages: true,
-                ReadMessageHistory: true
-            });
-
-        }
-
-        await message.channel.setTopic(null);
-
-        message.channel.send(
+        return message.channel.send(
             `🔓 Ticket libéré par ${message.author}`
         );
     }
