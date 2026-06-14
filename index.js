f7a32decbf8cc7bb51742389b5ddbaebef41410c
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-14 14:01:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,50 @@
-require("dotenv").config();
+require(“dotenv”).config();
 
-const fs = require("fs");
-const { Client, GatewayIntentBits } = require("discord.js");
+const fs = require(“fs”);
+const { Client, GatewayIntentBits } = require(“discord.js”);
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers,
-       GatewayIntentBits.GuildPresences,
-        GatewayIntentBits.GuildVoiceStates
-    ]
+intents: [
+GatewayIntentBits.Guilds,
+GatewayIntentBits.GuildMessages,
+GatewayIntentBits.MessageContent,
+GatewayIntentBits.GuildMembers,
+GatewayIntentBits.GuildPresences,
+GatewayIntentBits.GuildVoiceStates
+]
 });
 
 client.commands = new Map();
 
-const commandFolders = fs.readdirSync("./commands");
+const commandFolders = fs.readdirSync(”./commands”);
 
 for (const folder of commandFolders) {
-    const commandFiles = fs
-      .readdirSync(`./commands/${folder}`)
-        .filter(file => file.endsWith(".js"));
+const commandFiles = fs
+.readdirSync(./commands/${folder})
+.filter(file => file.endsWith(”.js”));
 
-    for (const file of commandFiles) {
-        const command = require(`./commands/${folder}/${file}`)
-        client.commands.set(command.name, command);
-    }
+for (const file of commandFiles) {
+    const command = require(`./commands/${folder}/${file}`);
+    client.commands.set(command.name, command);
 }
-console.log([...client.commands.keys()]);
 
-client.on("messageCreate", async (message) => {
+}
 
-    if (message.author.bot) return;
+client.once(“ready”, () => {
+console.log(✅ ${client.user.tag} est connecté !);
+});
 
-    const args = message.content.slice(1).trim().split(/ +/);
+client.on(“messageCreate”, async (message) => {
+
+if (message.author.bot) return;
+if (!message.content.startsWith("+")) return;
+const args = message.content.slice(1).trim().split(/ +/);
 const commandName = args.shift()?.toLowerCase();
-
 const command = client.commands.get(commandName);
-
 if (command) {
     return command.run(message, args);
 }
 
-client.once("ready", () => {
-console.log(`✅ ${client.user.tag} est connecté !`);
 });
-    });
 
 client.login(process.env.DISCORD_TOKEN);
