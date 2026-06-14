72cefb652a47ea60a78a710fe4393cf0f03ba76b
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-14 13:22:38 +0000

    Update close.js

--- commands/tickets/close.js
+++ commands/tickets/close.js
@@ -1,1 +1,21 @@
-[]
+module.exports = {
+    name: "close",
+
+    async run(message) {
+
+        if (!message.channel.name.startsWith("ticket-")) {
+            return message.reply("❌ Cette commande doit être utilisée dans un ticket.");
+        }
+
+        await message.channel.send(
+            "🔒 Fermeture du ticket dans 5 secondes..."
+        );
+
+        setTimeout(async () => {
+
+            await message.channel.delete()
+                .catch(() => {});
+
+        }, 5000);
+    }
+};
