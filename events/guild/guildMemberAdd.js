03787117da30dcc0fbd813df00f2cf81fb4dccbc
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-14 16:50:13 +0000

    Update guildMemberAdd.js

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,1 +1,26 @@
-// Guild event
+module.exports = async (member) => {
+
+    const sevenDays = 7 * 24 * 60 * 60 * 1000;
+
+    const accountAge =
+        Date.now() - member.user.createdTimestamp;
+
+    if (accountAge < sevenDays) {
+
+        try {
+            await member.send(
+                `Coucou ${member},
+
+Tu as été banni automatiquement de Shiiiro car ton compte est trop récent.
+
+Pour demander un bypass, rejoins ce serveur et ouvre un ticket :
+
+https://discord.gg/shiiro`
+            );
+        } catch (err) {}
+
+        await member.ban({
+            reason: "Compte de moins de 7 jours."
+        });
+    }
+};
