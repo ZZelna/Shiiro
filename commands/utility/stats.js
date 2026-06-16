89c54325ef9323831a67d0812a5378bb144727bd
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-16 20:38:57 +0200

    Update stats.js

--- commands/utility/stats.js
+++ commands/utility/stats.js
@@ -1,1 +1,61 @@
-[]
+const Stats =
+    require("../../systems/stats");
+
+const {
+    EmbedBuilder
+} = require("discord.js");
+
+module.exports = {
+
+    name: "stats",
+
+    async run(message) {
+
+        const target =
+            message.mentions.users.first()
+            || message.author;
+
+        const userStats =
+            await Stats.findOne({
+                userId: target.id
+            });
+
+        const embed =
+            new EmbedBuilder()
+
+            .setColor("Blue")
+
+            .setTitle(
+                "📊 Statistiques"
+            )
+
+            .setThumbnail(
+                target.displayAvatarURL({
+                    dynamic: true
+                })
+            )
+
+            .addFields(
+                {
+                    name: "💬 Messages",
+                    value: `${
+                        userStats?.messages || 0
+                    }`,
+                    inline: true
+                }
+            )
+
+            .setFooter({
+                text:
+                    target.username
+            })
+
+            .setTimestamp();
+
+        return message.reply({
+            embeds: [embed]
+        });
+
+    }
+
+};
