eb02f91b2370c2e2be26b7cd80595f2b605faf79
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-16 20:33:24 +0200

    Update stats.js

--- systems/stats.js
+++ systems/stats.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
 
 const statsSchema = new mongoose.Schema({
+
     userId: String,
+
     messages: {
         type: Number,
         default: 0
     }
+
 });
 
 module.exports =
-    mongoose.model("Stats", statsSchema);
+    mongoose.model(
+        "Stats",
+        statsSchema
+    );
