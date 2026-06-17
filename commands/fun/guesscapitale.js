abf3256dc3b544f5c4f8edef374d5c478af2a19e
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-17 16:30:01 +0200

    Update guesscapitale.js

--- commands/fun/guesscapitale.js
+++ commands/fun/guesscapitale.js
@@ -1,1 +1,163 @@
-[]
+const {
+    EmbedBuilder
+} = require("discord.js");
+
+const questions = [
+    {
+        pays: "France",
+        capitale: "Paris"
+    },
+    {
+        pays: "Espagne",
+        capitale: "Madrid"
+    },
+    {
+        pays: "Italie",
+        capitale: "Rome"
+    },
+    {
+        pays: "Allemagne",
+        capitale: "Berlin"
+    },
+    {
+        pays: "Portugal",
+        capitale: "Lisbonne"
+    },
+    {
+        pays: "Belgique",
+        capitale: "Bruxelles"
+    },
+    {
+        pays: "Pays-Bas",
+        capitale: "Amsterdam"
+    },
+    {
+        pays: "Canada",
+        capitale: "Ottawa"
+    },
+    {
+        pays: "Maroc",
+        capitale: "Rabat"
+    },
+    {
+        pays: "Algérie",
+        capitale: "Alger"
+    }
+];
+
+module.exports = {
+
+    name: "guesscapitale",
+
+    async run(message) {
+
+        const question =
+            questions[
+                Math.floor(
+                    Math.random() *
+                    questions.length
+                )
+            ];
+
+        const embed =
+            new EmbedBuilder()
+
+            .setColor("Blue")
+
+            .setTitle(
+                "🌍 Guess Capitale"
+            )
+
+            .setDescription(
+                `Quelle est la capitale de **${question.pays}** ?`
+            )
+
+            .setFooter({
+                text:
+                    "Tu as 30 secondes pour répondre"
+            })
+
+            .setTimestamp();
+
+        await message.channel.send({
+            embeds: [embed]
+        });
+
+        const filter = m =>
+            !m.author.bot;
+
+        const collector =
+            message.channel.createMessageCollector({
+                filter,
+                time: 30000
+            });
+
+        collector.on(
+            "collect",
+            async m => {
+
+                if (
+                    m.content.toLowerCase() ===
+                    question.capitale.toLowerCase()
+                ) {
+
+                    const winEmbed =
+                        new EmbedBuilder()
+
+                        .setColor(
+                            "Green"
+                        )
+
+                        .setTitle(
+                            "✅ Bonne réponse !"
+                        )
+
+                        .setDescription(
+                            `${m.author} a trouvé la capitale de **${question.pays}**.\n\nRéponse : **${question.capitale}**`
+                        );
+
+                    await message.channel.send({
+                        embeds: [winEmbed]
+                    });
+
+                    collector.stop();
+                }
+
+            }
+        );
+
+        collector.on(
+            "end",
+            async (_, reason) => {
+
+                if (
+                    reason ===
+                    "time"
+                ) {
+
+                    const loseEmbed =
+                        new EmbedBuilder()
+
+                        .setColor(
+                            "Red"
+                        )
+
+                        .setTitle(
+                            "⏰ Temps écoulé"
+                        )
+
+                        .setDescription(
+                            `Personne n'a trouvé.\n\nRéponse : **${question.capitale}**`
+                        );
+
+                    await message.channel.send({
+                        embeds: [loseEmbed]
+                    });
+
+                }
+
+            }
+        );
+
+    }
+};
