5a82eb422eb8581672df818518a0e323b6a9cf6d
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-17 17:50:15 +0200

    Update guessbrand.js

--- commands/fun/guessbrand.js
+++ commands/fun/guessbrand.js
@@ -1,1 +1,123 @@
-[]
+const {
+    EmbedBuilder,
+    AttachmentBuilder
+} = require("discord.js");
+
+const brands =
+    require("../../data/brands.json");
+
+const path = require("path");
+
+module.exports = {
+
+    name: "guessbrand",
+
+    async run(message) {
+
+        const question =
+            brands[
+                Math.floor(
+                    Math.random() *
+                    brands.length
+                )
+            ];
+
+        const file =
+            new AttachmentBuilder(
+                path.join(
+                    __dirname,
+                    "../../assets/logos",
+                    question.image
+                )
+            );
+
+        const embed =
+            new EmbedBuilder()
+
+            .setColor("Blue")
+
+            .setTitle(
+                "🏷️ Guess Brand"
+            )
+
+            .setDescription(
+                "Quelle est cette marque ?"
+            )
+
+            .setImage(
+                `attachment://${question.image}`
+            )
+
+            .setFooter({
+                text:
+                    "30 secondes"
+            });
+
+        await message.channel.send({
+            embeds: [embed],
+            files: [file]
+        });
+
+        const collector =
+            message.channel.createMessageCollector({
+                filter: m => !m.author.bot,
+                time: 30000
+            });
+
+        collector.on(
+            "collect",
+            async m => {
+
+                if (
+                    m.content.toLowerCase() ===
+                    question.brand.toLowerCase()
+                ) {
+
+                    collector.stop();
+
+                    return message.channel.send({
+                        embeds: [
+                            new EmbedBuilder()
+                            .setColor("Green")
+                            .setTitle(
+                                "✅ Bonne réponse"
+                            )
+                            .setDescription(
+                                `${m.author} a trouvé **${question.brand}**`
+                            )
+                        ]
+                    });
+
+                }
+
+            }
+        );
+
+        collector.on(
+            "end",
+            async (_, reason) => {
+
+                if (
+                    reason === "time"
+                ) {
+
+                    await message.channel.send({
+                        embeds: [
+                            new EmbedBuilder()
+                            .setColor("Red")
+                            .setTitle(
+                                "⏰ Temps écoulé"
+                            )
+                            .setDescription(
+                                `Réponse : **${question.brand}**`
+                            )
+                        ]
+                    });
+
+                }
+
+            }
+        );
+
+    }
+};
