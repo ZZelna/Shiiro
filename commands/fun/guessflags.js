5dd29c5acdb87f5c3de3f0b0c2981a77989ac822
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-18 19:09:59 +0200

    Update guessflags.js

--- commands/fun/guessflags.js
+++ commands/fun/guessflags.js
@@ -1,1 +1,33 @@
-[]
+client.on("messageCreate", async (message) => {
+    if (message.content === ".guessflags") {
+
+        const flags = require("./data/flags.json");
+        const random = flags[Math.floor(Math.random() * flags.length)];
+
+        const embed = {
+            title: "🌍 Guess The Flag",
+            description: "Quel pays correspond à ce drapeau ?",
+            image: {
+                url: `https://monsite.com/flags/${random.image}`
+            },
+            color: 0x3498db
+        };
+
+        await message.channel.send({ embeds: [embed] });
+
+        const filter = m => !m.author.bot;
+        const collector = message.channel.createMessageCollector({
+            filter,
+            time: 15000,
+            max: 1
+        });
+
+        collector.on("collect", m => {
+            if (m.content.toLowerCase() === random.country.toLowerCase()) {
+                m.reply("✅ Bonne réponse !");
+            } else {
+                m.reply(`❌ Mauvaise réponse ! La réponse était **${random.country}**.`);
+            }
+        });
+    }
+});
