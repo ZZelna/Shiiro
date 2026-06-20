c303ad4ea52b94b266f80b5ee49cc0af4674a648
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-20 11:58:24 +0200

    Update guesscountry.js

--- commands/fun/guesscountry.js
+++ commands/fun/guesscountry.js
@@ -1,1 +1,113 @@
-[]
+const {
+    EmbedBuilder
+} = require("discord.js");
+
+const countries =
+require("../../data/countries.json");
+
+module.exports = {
+
+    name: "guesscountry",
+
+    async run(message) {
+
+        const question =
+            countries[
+                Math.floor(
+                    Math.random() *
+                    countries.length
+                )
+            ];
+
+        const embed =
+            new EmbedBuilder()
+
+            .setColor("#5865F2")
+
+            .setTitle(
+                "🌍 Guess Country"
+            )
+
+            .setDescription(
+`📖 Description :
+
+${question.description}
+
+⏱️ Vous avez 30 secondes !`
+            );
+
+        await message.channel.send({
+            embeds: [embed]
+        });
+
+        const collector =
+            message.channel.createMessageCollector({
+                filter: m =>
+                    !m.author.bot,
+                time: 30000
+            });
+
+        collector.on(
+            "collect",
+            async m => {
+
+                if (
+                    m.content.toLowerCase() ===
+                    question.country.toLowerCase()
+                ) {
+
+                    await message.channel.send({
+                        embeds: [
+                            new EmbedBuilder()
+                            .setColor("#57F287")
+                            .setTitle(
+                                "✅ Bonne réponse"
+                            )
+                            .setDescription(
+`${m.author} a trouvé !
+
+🌍 Pays :
+**${question.country}**`
+                            )
+                        ]
+                    });
+
+                    collector.stop();
+
+                }
+
+            }
+        );
+
+        collector.on(
+            "end",
+            async (_, reason) => {
+
+                if (
+                    reason ===
+                    "time"
+                ) {
+
+                    await message.channel.send({
+                        embeds: [
+                            new EmbedBuilder()
+                            .setColor("#ED4245")
+                            .setTitle(
+                                "⏰ Temps écoulé"
+                            )
+                            .setDescription(
+`🌍 Réponse :
+
+**${question.country}**`
+                            )
+                        ]
+                    });
+
+                }
+
+            }
+        );
+
+    }
+
+};
