9bd74e12c773d3d02508a87baf195153d20f9644
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-21 21:15:42 +0200

    Update stats.js

--- commands/utility/stats.js
+++ commands/utility/stats.js
@@ -3,68 +3,82 @@ const createCard = require("../../utils/statsCard");
 const { AttachmentBuilder } = require("discord.js");
 
 module.exports = {
+name: "stats",
+description: "Affiche les statistiques d’un membre",
+   
+async run(message) {
 
-    name: "stats",
+    const target =
+        message.mentions.users.first() ||
+        message.author;
 
-    async run(message) {
+    const guildMember =
+        message.guild.members.cache.get(
+            target.id
+        );
 
-        const target =
-            message.mentions.users.first() ||
-            message.author;
+    const userStats =
+        await Stats.findOne({
+            userId: target.id
+        });
 
-        const userStats =
-            await Stats.findOne({
-                userId: target.id
-            });
+    let messages7d = 0;
+    let messages14d = 0;
 
-        let messages7d = 0;
-        let messages14d = 0;
+    const today =
+        new Date();
 
-        const today = new Date();
+    if (userStats?.dailyMessages) {
 
-        if (userStats?.dailyMessages) {
+        for (
+            const [date, count]
+            of userStats.dailyMessages
+        ) {
 
-            for (const [date, count] of userStats.dailyMessages) {
+            const diff =
+                (
+                    today -
+                    new Date(date)
+                ) /
+                86400000;
 
-                const diff =
-                    (today - new Date(date))
-                    / 86400000;
+            if (diff <= 7)
+                messages7d += count;
 
-                if (diff <= 7)
-                    messages7d += count;
-
-                if (diff <= 14)
-                    messages14d += count;
-
-            }
+            if (diff <= 14)
+                messages14d += count;
 
         }
 
-        const image =
-            await createCard(
-                target,
-                {
-                    messages:
-                        userStats?.messages || 0,
-
-                    messages7d,
-
-                    messages14d
-                }
-            );
-
-        const attachment =
-            new AttachmentBuilder(
-                image,
-                {
-                    name: "stats.png"
-                }
-            );
-
-        return message.reply({
-            files: [attachment]
-        });
-
     }
 
-};
+    const image =
+        await createCard(
+            guildMember,
+            {
+                messages:
+                    userStats?.messages || 0,
+
+                messagesToday:
+                    userStats?.messagesToday || 0,
+
+                messages7d,
+
+                messages14d
+            }
+        );
+
+    const attachment =
+        new AttachmentBuilder(
+            image,
+            {
+                name: "stats.png"
+            }
+        );
+
+    return message.reply({
+        files: [attachment]
+    });
+
+}
+    };
