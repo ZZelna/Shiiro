1d3ff5c7377169c75af1efaf257144d61efcb311
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-22 19:47:30 +0200

    Update sticky.js

--- events/sticky.js
+++ events/sticky.js
@@ -1,14 +1,20 @@
 const stickyMessages = {
 
-    "1514677335293563012": `**Uniquement des commandes ici !!! Pas de discussions !!!**`,
+    "1514677335293563012": `__**Stickied Message:**__
 
-    "1506684777678377001": `**Uniquement des commandes ici !!! Pas de discussions !!!**`,
+**Uniquement des commandes ici !!! Pas de discussions !!!**`,
 
-    "1506688699893809152": `**Uniquement des images ou des vidéos ici !!! Pas de discussions !!!**`,
+    "1506684777678377001": `__**Stickied Message:**__
+
+**Uniquement des commandes ici !!! Pas de discussions !!!**`,
+
+    "1506688699893809152": `__**Stickied Message:**__
+
+**Uniquement des images ou des vidéos ici !!! Pas de discussions !!!**`,
 
     "1506939756058120202": `__**Stickied Message:**__
 
-un chiffre a la fois merci !!!!`,
+un chiffre à la fois merci !!!!`,
 
     "1506960053368782919": `__**Stickied Message:**__
 
@@ -16,22 +22,31 @@ uniquement des suggestions ici 👈
 
 Les owners répondront aux suggestions au plus vite !!!`,
 
-    "1510484744767410206": `**Vous pouvez bump le serveur toutes les 2h merci à tous ceux qui le font !!!**`,
+    "1510484744767410206": `__**Stickied Message:**__
 
-    "1509881074417799168": `**uniquement des citations ici !!!!**`,
+**Vous pouvez bump le serveur toutes les 2h merci à tous ceux qui le font !!!**`,
 
-    "1506944756524253284": `## ✔️  __Demande de bannissement :__
--# Envoyez les preuves dans un seul message.
+    "1509881074417799168": `__**Stickied Message:**__
+
+**uniquement des citations ici !!!!**`,
+
+    "1506944756524253284": `__**Stickied Message:**__
+
+## ✔️ Demande de bannissement
+
+- Envoyez les preuves dans un seul message.
 
 || <@&1507082580414173234> ||
 
-\`\`👤  Pseudo / ID :
+\`\`👤 Pseudo / ID :
 
 \`\`❓ Raison du bannissement :
 
 \`\`📌 Preuve (screen) :`,
 
-    "1508433969711153203": `**Personne :** …
+    "1508433969711153203": `__**Stickied Message:**__
+
+**Personne :** …
 
 **Rank :** …
 
@@ -39,31 +54,38 @@ Les owners répondront aux suggestions au plus vite !!!`,
 
 **Par :** …`,
 
- "1506942496884785152": `__**Stickied Message:**__
+    "1506942496884785152": `__**Stickied Message:**__
 
 **Bienvenue dans le salon confession de Shiiro**
 
-Ici la règle principale est l’anonymat : Tu peux donc envoyer tes confessions en toute liberté mais veille à ne pas trop en abuser non plus 😰
+Ici la règle principale est l’anonymat : tu peux envoyer tes confessions en toute liberté mais veille à ne pas trop en abuser 😰
 
-- **Ca peut être une anecdote**
+- **Anecdotes**
 - **Secrets**
 - **Révélations**
 - **Crush**
 - **Blagues**
 
-Bref tout ce qui te traverse à l’esprit
+Bref, tout ce qui te traverse l’esprit.
 
 C’est ici : 👈
 https://ngl.link/jestonoff11941`,
+};
    
-"1507164423276466268": `# Durées de Jail
+"1507164423276466268": `__**Stickied Message:**__
+
+#  **Commande : /jail**
+
+# Durées de Jail
 
 - Spam = 5 minutes
 - Insultes = 5 minutes (+5 minutes si récidive)
 - Messages offensants = 10 minutes
 - Leak / Dox / Swatt / Menaces = 60 minutes (ATT BAN)
 - NSFW = 60 minutes (ATT BAN)`,
-"1507078497959546983": `# Format :
+"1507078497959546983": `__**Stickied Message:**__
+
+# Format :
 
 ~~                      ~~
 
