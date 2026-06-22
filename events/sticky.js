ffd8d0bf1bffe092abe439a35a283ff72de3f63e
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-22 19:55:23 +0200

    Update sticky.js

--- events/sticky.js
+++ events/sticky.js
@@ -74,15 +74,16 @@ https://ngl.link/jestonoff11941`,
    
 "1507164423276466268": `__**Stickied Message:**__
 
-#  **Commande : /jail**
+## Commande : /jail
 
-# Durées de Jail
+### Durées de Jail
 
 - Spam = 5 minutes
 - Insultes = 5 minutes (+5 minutes si récidive)
 - Messages offensants = 10 minutes
 - Leak / Dox / Swatt / Menaces = 60 minutes (ATT BAN)
 - NSFW = 60 minutes (ATT BAN)`,
+    
 "1507078497959546983": `__**Stickied Message:**__
 
 # Format :
