decc3257fd8578d94cd434105eba06d1ec256413
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-23 16:25:16 +0200

    Create bypass.js

---
+++ slashCommands/bypass.js
@@ -0,0 +1,86 @@
+const fs = require("fs");
+const path = require("path");
+const {
+SlashCommandBuilder,
+EmbedBuilder
+} = require("discord.js");
+
+const bypassData = require("../data/bypass.json");
+
+const allowedRoles = [
+"1506674274826584284",
+"1507082580414173234"
+];
+
+module.exports = {
+data: new SlashCommandBuilder()
+.setName("bypass")
+.setDescription("Ajoute un utilisateur à la liste bypass")
+.addUserOption(option =>
+option
+.setName("membre")
+.setDescription("Membre à ajouter")
+.setRequired(true)
+),
+async execute(interaction) {
+
+    const hasPermission =
+        interaction.member.roles.cache.some(role =>
+            allowedRoles.includes(role.id)
+        );
+
+    if (!hasPermission) {
+        return interaction.reply({
+            content: "❌ Vous n'avez pas la permission d'utiliser cette commande.",
+            ephemeral: true
+        });
+    }
+
+    const member =
+        interaction.options.getUser("membre");
+
+    if (!member) {
+        return interaction.reply({
+            content: "❌ Utilisateur introuvable.",
+            ephemeral: true
+        });
+    }
+
+    if (!bypassData.users) {
+        bypassData.users = [];
+    }
+
+    if (!bypassData.users.includes(member.id)) {
+
+        bypassData.users.push(member.id);
+
+        fs.writeFileSync(
+            path.join(
+                __dirname,
+                "../data/bypass.json"
+            ),
+            JSON.stringify(
+                bypassData,
+                null,
+                4
+            )
+        );
+    }
+
+    const embed = new EmbedBuilder()
+        .setColor("Green")
+        .setTitle("✅ By-pass ajouté")
+        .setDescription(
+            `${member} a été ajouté à la liste bypass.`
+        )
+        .addFields({
+            name: "👮 Responsable",
+            value: interaction.user.tag
+        })
+        .setTimestamp();
+
+    await interaction.reply({
+        embeds: [embed]
+    });
+}
+  };
