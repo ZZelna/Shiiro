7e9d79ce9c05e62585af0b43b76c3dbbb1860fcb
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-24 16:42:33 +0200

    Update memberLeave.js

--- events/logs/memberLeave.js
+++ events/logs/memberLeave.js
@@ -1,64 +1,21 @@
-const {
-    EmbedBuilder
-} = require("discord.js");
-
 module.exports = async (member) => {
 
     const logs =
         member.guild.channels.cache.get(
-            "1516880092725248160"
+            "1516880006075383909"
         );
 
     if (!logs) return;
 
-    const embed =
-        new EmbedBuilder()
-
-        .setColor("#ED4245")
-
-        .setAuthor({
-            name:
-                `${member.user.tag} (${member.id})`,
-            iconURL:
-                member.user.displayAvatarURL({
-                    dynamic: true
-                })
-        })
-
-        .setTitle(
-            "🔴 Départ d'un membre"
-        )
-
-        .setDescription(
-            `${member.user.tag} a quitté le serveur.`
-        )
-
-        .addFields(
-            {
-                name: "👤 Utilisateur",
-                value:
-                    member.user.tag,
-                inline: true
-            },
-            {
-                name: "🆔 ID",
-                value:
-                    member.id,
-                inline: true
-            }
-        )
-
-        .setThumbnail(
-            member.user.displayAvatarURL({
-                dynamic: true,
-                size: 1024
-            })
-        )
-
-        .setTimestamp();
-
-    logs.send({
-        embeds: [embed]
+    await logs.send({
+        content:
+`\
+\`\`\`diff
+- Départ d'un membre.
+Utilisateur: ${member.user.tag} (ID: ${member.id})
+Action: Utilisateur a quitté le serveur. 🔴
+\`\`\`
+`
     });
 
 };
