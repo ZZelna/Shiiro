cc5a1a4026e43faa99e73d3474b2b730188a04e6
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-24 09:39:28 +0200

    Create addcoins.js

---
+++ slashCommands/addcoins.js
@@ -0,0 +1,98 @@
+const {
+    SlashCommandBuilder,
+    EmbedBuilder
+} = require("discord.js");
+
+const CasinoProfile =
+    require("../models/CasinoProfile");
+
+const allowedRoles = [
+    "1506674274826584284",
+    "1506709088451690708"
+];
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("addcoins")
+        .setDescription(
+            "Ajouter des yens à un joueur"
+        )
+        .addUserOption(option =>
+            option
+                .setName("utilisateur")
+                .setDescription(
+                    "Joueur"
+                )
+                .setRequired(true)
+        )
+        .addIntegerOption(option =>
+            option
+                .setName("montant")
+                .setDescription(
+                    "Montant"
+                )
+                .setRequired(true)
+        ),
+
+    async execute(interaction) {
+
+        if (
+            !interaction.member.roles.cache.some(
+                role =>
+                    allowedRoles.includes(
+                        role.id
+                    )
+            )
+        ) {
+            return interaction.reply({
+                content:
+                    "❌ Permission refusée.",
+                ephemeral: true
+            });
+        }
+
+        const user =
+            interaction.options.getUser(
+                "utilisateur"
+            );
+
+        const amount =
+            interaction.options.getInteger(
+                "montant"
+            );
+
+        let profile =
+            await CasinoProfile.findOne({
+                userId: user.id
+            });
+
+        if (!profile) {
+
+            profile =
+                await CasinoProfile.create({
+                    userId: user.id,
+                    yens: 0,
+                    gifts: 0
+                });
+
+        }
+
+        profile.yens += amount;
+
+        await profile.save();
+
+        const embed =
+            new EmbedBuilder()
+                .setColor("Green")
+                .setTitle(
+                    "💴 Yens ajoutés"
+                )
+                .setDescription(
+                    `✅ ${user} reçoit **${amount.toLocaleString()} ¥**`
+                );
+
+        return interaction.reply({
+            embeds: [embed]
+        });
+    }
+};
