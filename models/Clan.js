eca7efebc2d4f62e3f5705567a5cc68cf97a5d3e
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-24 09:56:55 +0200

    Update Clan.js

--- models/Clan.js
+++ models/Clan.js
@@ -4,13 +4,25 @@ module.exports = mongoose.model(
     "Clan",
     new mongoose.Schema({
         name: String,
+
         ownerId: String,
-        members: [String],
+
+        members: {
+            type: [String],
+            default: [],
+            validate: [
+                arr => arr.length <= 10,
+                "Un clan ne peut pas dépasser 10 membres."
+            ]
+        },
+
         totalYens: {
             type: Number,
             default: 0
         },
+
         channelId: String,
+
         createdAt: {
             type: Date,
             default: Date.now
