d7e77e56e1b105d3d85f42a6f59ac360d4d2a182
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-25 12:10:08 +0200

    Create autoQuiz.js

---
+++ systems/autoQuiz.js
@@ -0,0 +1,59 @@
+let quizRunning = false;
+
+module.exports = function startAutoQuiz(client) {
+
+    setInterval(async () => {
+
+        if (quizRunning) return;
+
+        const channel =
+        client.channels.cache.get(
+            "1508491934547574814"
+        );
+
+        if (!channel) return;
+
+        quizRunning = true;
+
+        try {
+
+            const games = [
+                "guessanime",
+                "guessbrand",
+                "guesscountry",
+                "guesscapital"
+            ];
+
+            const randomGame =
+            games[
+                Math.floor(
+                    Math.random() *
+                    games.length
+                )
+            ];
+
+            await channel.send(
+                `🎯 Lancement automatique : **${randomGame}**`
+            );
+
+            // Plus tard :
+            // lancer le vrai mini-jeu ici
+
+        } catch (err) {
+
+            console.error(
+                "[AUTO QUIZ ERROR]",
+                err
+            );
+
+        }
+
+        setTimeout(() => {
+
+            quizRunning = false;
+
+        }, 30000);
+
+    }, 5 * 60 * 1000);
+
+};
