f424fe3ba8c8e9bf0a029aa4c6ae4812ff4dd15d
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-25 17:38:49 +0200

    Update VoiceStats.js

--- models/VoiceStats.js
+++ models/VoiceStats.js
@@ -5,18 +5,10 @@ module.exports =
 mongoose.model(
     "VoiceStats",
     new mongoose.Schema({
-
         userId: String,
-
         totalSeconds: {
             type: Number,
             default: 0
-        },
-
-        updatedAt: {
-            type: Date,
-            default: Date.now
         }
-
     })
 );
