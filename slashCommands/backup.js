a578db358351a548c08bfd921ecc86a660d3465e
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-26 09:14:19 +0200

    Update backup.js

--- slashCommands/backup.js
+++ slashCommands/backup.js
@@ -36,118 +36,140 @@ module.exports = {
             interaction.guild;
 
         // RÔLES
-        const roles =
-            guild.roles.cache
-                .filter(
-                    role => role.id !== guild.id
-                )
-                .sort(
-                    (a, b) =>
-                        b.position - a.position
-                )
-                .map(role => ({
-                    name: role.name,
-                    color: role.color,
-                    permissions:
-                        role.permissions.bitfield.toString(),
-                    hoist: role.hoist,
-                    mentionable:
-                        role.mentionable,
-                    position:
-                        role.position
-                }));
+        const roles = guild.roles.cache
+    .filter(role => role.id !== guild.id)
+    .sort((a, b) => b.position - a.position)
+    .map(role => ({
+        id: role.id,
+        name: role.name,
+        color: role.color,
+        permissions: role.permissions.bitfield.toString(),
+        hoist: role.hoist,
+        mentionable: role.mentionable,
+        position: role.position,
+        icon: role.iconURL(),
+        unicodeEmoji: role.unicodeEmoji,
+        managed: role.managed
+    }));
 
         // SALONS
-        const channels =
-            guild.channels.cache
-                .sort(
-                    (a, b) =>
-                        a.position - b.position
-                )
-                .map(channel => ({
+     const channels = guild.channels.cache
+    .sort((a, b) => a.rawPosition - b.rawPosition)
+    .map(channel => ({
 
-                    name: channel.name,
+        id: channel.id,
+        name: channel.name,
+        type: channel.type,
 
-                    type: channel.type,
+        position: channel.rawPosition,
 
-                    position:
-                        channel.position,
+        parentId: channel.parentId,
 
-                    parent:
-                        channel.parent
-                            ?.name || null,
+        topic: channel.topic || null,
 
-                    topic:
-                        channel.topic || null,
+        nsfw: channel.nsfw || false,
 
-                    nsfw:
-                        channel.nsfw || false,
+        rateLimitPerUser:
+            channel.rateLimitPerUser || 0,
 
-                    rateLimitPerUser:
-                        channel.rateLimitPerUser || 0,
+        bitrate:
+            channel.bitrate || null,
 
-                    bitrate:
-                        channel.bitrate || 64000,
+        userLimit:
+            channel.userLimit || null,
 
-                    userLimit:
-                        channel.userLimit || 0,
+        rtcRegion:
+            channel.rtcRegion || null,
 
-                    permissionOverwrites:
-                        channel.permissionOverwrites.cache.map(
-                            overwrite => ({
+        videoQualityMode:
+            channel.videoQualityMode || null,
 
-                                id:
-                                    overwrite.id,
+        defaultAutoArchiveDuration:
+            channel.defaultAutoArchiveDuration || null,
 
-                                type:
-                                    overwrite.type,
+        permissionOverwrites:
+            channel.permissionOverwrites.cache.map(overwrite => ({
 
-                                allow:
-                                    overwrite.allow.bitfield.toString(),
+                id: overwrite.id,
 
-                                deny:
-                                    overwrite.deny.bitfield.toString()
+                type: overwrite.type,
 
-                            })
-                        )
+                allow:
+                    overwrite.allow.bitfield.toString(),
 
-                }));
+                deny:
+                    overwrite.deny.bitfield.toString()
 
-        await Backup.findOneAndUpdate(
+            }))
 
-            {
-                guildId:
-                    guild.id
-            },
+    }));
+     // EMOJIS
+const emojis = guild.emojis.cache.map(emoji => ({
+    name: emoji.name,
+    url: emoji.url,
+    animated: emoji.animated
+}));
+// STICKERS
+const stickers = guild.stickers.cache.map(sticker => ({
+    name: sticker.name,
+    description: sticker.description,
+    tags: sticker.tags,
+    url: sticker.url
+}));
+// PARAMÈTRES DU SERVEUR
+const settings = {
 
-            {
+    name: guild.name,
 
-                guildId:
-                    guild.id,
+    icon: guild.iconURL({
+        extension: "png",
+        size: 4096
+    }),
 
-                guildName:
-                    guild.name,
+    banner: guild.bannerURL({
+        extension: "png",
+        size: 4096
+    }),
 
-                guildIcon:
-                    guild.iconURL(),
+    description: guild.description,
 
-                createdBy:
-                    interaction.user.id,
+    verificationLevel:
+        guild.verificationLevel,
 
-                createdAt:
-                    new Date(),
+    explicitContentFilter:
+        guild.explicitContentFilter,
 
-                roles,
+    defaultNotifications:
+        guild.defaultMessageNotifications,
 
-                channels
+    preferredLocale:
+        guild.preferredLocale,
 
-            },
+    afkTimeout:
+        guild.afkTimeout
+};
+await Backup.findOneAndUpdate(
+    {
+        guildId: guild.id
+    },
+    {
+        guildId: guild.id,
+        guildName: guild.name,
+        guildIcon: guild.iconURL(),
+        createdBy: interaction.user.id,
+        createdAt: new Date(),
 
-            {
-                upsert: true
-            }
+        settings,
+        roles,
+        channels,
+        emojis,
+        stickers
 
-        );
+    },
+    {
+        upsert: true
+    }
+);
 
         await interaction.editReply(
             "✅ Backup terminé avec succès."
