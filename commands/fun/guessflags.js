4fcf578525401095d5e859c7453e680ca2dd83b9
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-26 10:12:53 +0200

    Update guessflags.js

--- commands/fun/guessflags.js
+++ commands/fun/guessflags.js
@@ -3,7 +3,18 @@ const flags = require("../../data/flags.json");
 module.exports = {
     name: "guessflags",
 
-    async run(message, args) {
+    run: async (message, args, options = {}) => {
+
+    if (!message.member && !options.auto) return;
+
+    if (!options.auto) {
+        const roleAllowed = "1506674274826584284";
+        if (!message.member.roles.cache.has(roleAllowed)) {
+            return message.reply("❌ Tu n'as pas la permission d'utiliser ce mini-jeu.");
+        }
+    }
+    // reste inchangé...
+
 
         const random =
             flags[Math.floor(Math.random() * flags.length)];
