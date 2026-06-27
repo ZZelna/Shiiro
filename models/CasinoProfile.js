c102136d2d4d5a6ba70d206a7dbf075007717ae3
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-27 22:58:55 +0200

    Update CasinoProfile.js

--- models/CasinoProfile.js
+++ models/CasinoProfile.js
@@ -1,33 +1,32 @@
 const mongoose = require("mongoose");
 
 module.exports = mongoose.model(
-    "CasinoProfile",
-    new mongoose.Schema({
-        userId: String,
-        yens: {
-            type: Number,
-            default: 1000
-        },
-        gifts: {
-            type: Number,
-            default: 0
-        },
-        boostMultiplier: {
-    type: Number,
-    default: 1
-},
-
-boostEnd: {
-    type: Date,
-    default: null
-},
-        lastDaily: {
-            type: Date,
-            default: null
-        },
-        lastClaim: {
-    type: Number,
-    default: 0
-}
-    })
+  "CasinoProfile",
+  new mongoose.Schema({
+    userId: String,
+    yens: {
+      type: Number,
+      default: 1000
+    },
+    gifts: {
+      type: Number,
+      default: 0
+    },
+    boostMultiplier: {
+      type: Number,
+      default: 1
+    },
+    boostEnd: {
+      type: Date,
+      default: null
+    },
+    lastDaily: {
+      type: Date,
+      default: null
+    },
+    lastClaim: {
+      type: Number,
+      default: 0
+    }
+  })
 );
