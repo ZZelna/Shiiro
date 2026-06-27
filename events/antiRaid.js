f332f30ca4ad399f2d13b81827f294ca0df35341
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-27 19:37:27 +0200

    Update antiRaid.js

--- events/antiRaid.js
+++ events/antiRaid.js
@@ -11,7 +11,7 @@ module.exports = async (member) => {
             });
 
             const log = member.guild.channels.cache.get(
-                "ID_LOGS"
+                "1520445114097598665"
             );
 
             if (log) {
