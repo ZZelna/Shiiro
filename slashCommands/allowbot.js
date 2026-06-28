049789efa2ea90aae656c05359d632958a308c96
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-28 17:26:27 +0200

    Update allowbot.js

--- slashCommands/allowbot.js
+++ slashCommands/allowbot.js
@@ -3,12 +3,12 @@ const {
     PermissionFlagsBits
 } = require("discord.js");
 
-const ShieldConfig = require("../models/ShieldConfig");
+const AllowedBot = require("../models/AllowedBot");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("allowbot")
-        .setDescription("Autorise un bot à rejoindre le serveur.")
+        .setDescription("Autorise un bot.")
         .addStringOption(option =>
             option
                 .setName("id")
@@ -21,33 +21,22 @@ module.exports = {
 
         const botId = interaction.options.getString("id");
 
-        let config = await ShieldConfig.findOne({
-            guildId: interaction.guild.id
-        });
+        const exists = await AllowedBot.findOne({ botId });
 
-        if (!config) {
-            config = await ShieldConfig.create({
-                guildId: interaction.guild.id,
-                allowedBots: []
-            });
-        }
-
-        if (!config.allowedBots)
-            config.allowedBots = [];
-
-        if (config.allowedBots.includes(botId)) {
+        if (exists) {
             return interaction.reply({
                 content: "❌ Ce bot est déjà autorisé.",
                 ephemeral: true
             });
         }
 
-        config.allowedBots.push(botId);
-        await config.save();
-
-        interaction.reply({
-            content: `✅ Bot autorisé : \`${botId}\``
+        await AllowedBot.create({
+            botId,
+            addedBy: interaction.user.id
         });
 
+        await interaction.reply({
+            content: `✅ Bot autorisé : \`${botId}\``
+        });
     }
 };
