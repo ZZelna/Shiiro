838d9d97e3dde57c9e344a8c1c64d2dbcf292551
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-29 19:22:13 +0200

    Create games.js

---
+++ slashCommands/games.js
@@ -0,0 +1,81 @@
+const {
+    SlashCommandBuilder,
+    ActionRowBuilder,
+    StringSelectMenuBuilder
+} = require("discord.js");
+
+module.exports = {
+
+    data: new SlashCommandBuilder()
+        .setName("games")
+        .setDescription("Lance un mini-jeu sans récompense."),
+
+    async execute(interaction) {
+
+        const menu = new StringSelectMenuBuilder()
+
+            .setCustomId("games_menu")
+
+            .setPlaceholder("Choisissez un mini-jeu")
+
+            .addOptions(
+
+                {
+                    label: "Guess Anime",
+                    value: "guessanime",
+                    emoji: "🌸"
+                },
+
+                {
+                    label: "Guess Brand",
+                    value: "guessbrand",
+                    emoji: "🏷️"
+                },
+
+                {
+                    label: "Guess Couleur",
+                    value: "guesscouleur",
+                    emoji: "🎨"
+                },
+
+                {
+                    label: "Guess City",
+                    value: "guesscita",
+                    emoji: "🏙️"
+                },
+
+                {
+                    label: "Guess Country",
+                    value: "guesscountry",
+                    emoji: "🌍"
+                },
+
+                {
+                    label: "Guess Capitale",
+                    value: "guesscapitale",
+                    emoji: "🏛️"
+                },
+
+                {
+                    label: "Guess Flags",
+                    value: "guessflags",
+                    emoji: "🚩"
+                }
+
+            );
+
+        const row = new ActionRowBuilder()
+            .addComponents(menu);
+
+        await interaction.reply({
+
+            content:
+                "🎮 **Choisissez un mini-jeu.**\n*Aucune récompense ne sera donnée.*",
+
+            components: [row]
+
+        });
+
+    }
+
+};
