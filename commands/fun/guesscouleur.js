0268b36b6d4544ac0d256fc7610c9c8561ddd05a
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-29 19:41:43 +0200

    Update guesscouleur.js

--- commands/fun/guesscouleur.js
+++ commands/fun/guesscouleur.js
@@ -49,32 +49,48 @@ module.exports = {
             if (m.content.toLowerCase().trim() === color.name.toLowerCase()) {
                 collector.stop("found");
 
-                const isGift = Math.random() < 0.10;
-                let rewardText;
+               let rewardText = "🎮 Aucune récompense";
 
-                if (isGift) {
-                    await CasinoProfile.findOneAndUpdate(
-                        { userId: m.author.id },
-                        { $inc: { gifts: 1 } },
-                        { upsert: true }
-                    );
-                    rewardText = "🎁 1 Gift";
-                } else {
-                    const reward = Math.floor(Math.random() * 901) + 100;
-                    await CasinoProfile.findOneAndUpdate(
-                        { userId: m.author.id },
-                        { $inc: { yens: reward } },
-                        { upsert: true }
-                    );
-                    rewardText = `💴 ${reward} Yens`;
-                }
+if (options.reward !== false) {
+
+    const isGift = Math.random() < 0.10;
+
+    if (isGift) {
+
+        await CasinoProfile.findOneAndUpdate(
+            { userId: m.author.id },
+            { $inc: { gifts: 1 } },
+            { upsert: true }
+        );
+
+        rewardText = "🎁 1 Gift";
+
+    } else {
+
+        const reward = Math.floor(Math.random() * 901) + 100;
+
+        await CasinoProfile.findOneAndUpdate(
+            { userId: m.author.id },
+            { $inc: { yens: reward } },
+            { upsert: true }
+        );
+
+        rewardText = `💴 ${reward} Yens`;
+
+    }
+
+}
 
                 await m.reply({
                     embeds: [
                         new EmbedBuilder()
                             .setColor("#57F287")
                             .setTitle("✅ Bonne réponse")
-                            .setDescription(`La couleur était **${color.name}**.\n\nRécompense : ${rewardText}`)
+.setDescription(
+    options.reward === false
+        ? `La couleur était **${color.name}**.`
+        : `La couleur était **${color.name}**.\n\nRécompense : ${rewardText}`
+)
                             .setTimestamp()
                     ]
                 });
