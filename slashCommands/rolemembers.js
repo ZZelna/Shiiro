1f8d6a6c4abcff3b1c1419ad6632c2f895cd7fe2
ZZelna <eric.canisius1010101010@gmail.com>
2026-06-29 11:02:14 +0200

    Update rolemembers.js

--- slashCommands/rolemembers.js
+++ slashCommands/rolemembers.js
@@ -1,29 +1,115 @@
 const {
     SlashCommandBuilder
 } = require("discord.js");
+
 module.exports = {
+
     data: new SlashCommandBuilder()
         .setName("rolemembers")
-        .setDescription("Affiche les membres possédant un rôle.")
+        .setDescription("Affiche tous les membres possédant un rôle.")
         .addRoleOption(option =>
             option
                 .setName("role")
                 .setDescription("Le rôle à vérifier")
                 .setRequired(true)
         ),
+
     async execute(interaction) {
+
         const role =
             interaction.options.getRole("role");
+
         const members =
             [...role.members.values()];
-        if (members.length === 0) {
-            return interaction.reply({
-                content:
-`\\\`\\\`\\\`diff
-- Informations du rôle.
+
+        const footer =
+`Action: Liste récupérée.
+Demandé par: ${interaction.user.tag} (ID: ${interaction.user.id})`;
+
+        let page = 1;
+
+        let content =
+`- Informations du rôle.
+
 Rôle: ${role.name} (ID: ${role.id})
-Nombre de membres: 0
-Membres:
-Aucun membre.
-Action: Liste récupérée.
-Demandé par: ${interaction.user.tag} (ID: ${
+Nombre de membres: ${members.length}
+
+`;
+
+        let messages = [];
+
+        if (members.length === 0) {
+
+            content +=
+`Aucun membre.
+
+${footer}`;
+
+            return interaction.reply({
+
+                content:
+`\`\`\`diff
+${content}
+\`\`\``
+
+            });
+
+        }
+
+        for (const member of members) {
+
+            const line =
+`Membre #${page}
+Utilisateur: ${member.user.tag} (ID: ${member.id})
+
+`;
+
+            if (
+                (content + line + footer).length > 1900
+            ) {
+
+                messages.push(content + footer);
+
+                content =
+`- Informations du rôle.
+
+Rôle: ${role.name} (ID: ${role.id})
+Nombre de membres: ${members.length}
+
+`;
+
+            }
+
+            content += line;
+
+            page++;
+
+        }
+
+        messages.push(content + footer);
+
+        await interaction.reply({
+
+            content:
+`\`\`\`diff
+${messages.shift()}
+\`\`\``
+
+        });
+
+        for (const msg of messages) {
+
+            await interaction.followUp({
+
+                content:
+`\`\`\`diff
+${msg}
+\`\`\``
+
+            });
+
+        }
+
+    }
+
+};
