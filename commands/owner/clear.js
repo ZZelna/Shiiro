2d1301bb922dabac531d6eaf11e7b27a60f61d0b
ZZelna <eric.canisius1010101010@gmail.com>
2026-07-01 15:34:46 +0200

    Update clear.js

--- commands/owner/clear.js
+++ commands/owner/clear.js
@@ -4,28 +4,21 @@ module.exports = {
     async run(message, args) {
 
         const allowedRoles = [
-  "1506674274826584284",
-  "1521596407968960613"
-];
+            "1506674274826584284",
+            "1521596407968960613"
+        ];
 
-        if (!message.member.roles.cache.has(allowedRole)) {
-            return message.reply(
-                "❌ Vous n'avez pas la permission."
-            );
+        if (!message.member.roles.cache.some(role => allowedRoles.includes(role.id))) {
+            return message.reply("❌ Vous n'avez pas la permission.");
         }
 
         const amount = parseInt(args[0]);
 
         if (!amount || amount < 1 || amount > 100) {
-            return message.reply(
-                "❌ Utilisation : +clear 1-100"
-            );
+            return message.reply("❌ Utilisation : +clear <1-100>");
         }
 
-        await message.channel.bulkDelete(
-            amount,
-            true
-        );
+        await message.channel.bulkDelete(amount, true);
 
         const msg = await message.channel.send(
             `🗑️ ${amount} messages supprimés.`
