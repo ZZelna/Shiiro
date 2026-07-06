aea6fd801aa704e2fd0507f313440b055a7f9062
ZZelna <eric.canisius1010101010@gmail.com>
2026-07-06 19:19:57 +0200

    Update welcome.js

--- events/welcome.js
+++ events/welcome.js
@@ -15,7 +15,7 @@ module.exports = async (member) => {
             `Bienvenue ${member} !\n\nGrâce à toi, nous sommes désormais **${member.guild.memberCount} membres**.`
         )
         .setThumbnail(member.user.displayAvatarURL())
-        .setImage("https://media.gifdb.com/gorgeous-frieren-reading-drinking-tea-u6dxq1urw11ozpa8.gif")
+        .setImage("https://media.gifdb.com/frieren-magical-spell-zoltraak-z7qksirmj99dwxmu.gif")
         .setTimestamp();
 
     await channel.send({
