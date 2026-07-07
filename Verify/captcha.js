a1993a37b9d6e54b59242a12cb1542fecc56c4a6
ZZelna <eric.canisius1010101010@gmail.com>
2026-07-07 19:05:05 +0200

    Update captcha.js

--- Verify/captcha.js
+++ Verify/captcha.js
@@ -1,17 +1,23 @@
 const axios = require("axios");
 
-async function verifyCaptcha(token) {
+async function verifyCaptcha(token, ip = null) {
 
     if (!token) return false;
 
     try {
 
+        const params = new URLSearchParams();
+
+        params.append("secret", process.env.TURNSTILE_SECRET_KEY);
+        params.append("response", token);
+
+        if (ip) {
+            params.append("remoteip", ip);
+        }
+
         const { data } = await axios.post(
             "https://challenges.cloudflare.com/turnstile/v0/siteverify",
-            new URLSearchParams({
-                secret: process.env.TURNSTILE_SECRET_KEY,
-                response: token
-            }),
+            params,
             {
                 headers: {
                     "Content-Type": "application/x-www-form-urlencoded"
@@ -23,7 +29,7 @@ async function verifyCaptcha(token) {
 
     } catch (err) {
 
-        console.log("Erreur Turnstile :", err.message);
+        console.error("Erreur Turnstile :", err.response?.data || err.message);
 
         return false;
 
