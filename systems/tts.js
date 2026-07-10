e2c696798a844008967943a4cd5ab2d29366d0f4
ZZelna <eric.canisius1010101010@gmail.com>
2026-07-10 16:04:52 +0200

    Update tts.js

--- systems/tts.js
+++ systems/tts.js
@@ -1,27 +1,21 @@
 const { EdgeTTS } = require("@andresaya/edge-tts");
 
 async function generateVoice(text) {
-
     try {
-
         const tts = new EdgeTTS();
 
-        const audio = await tts.synthesize(
+        const result = await tts.synthesize(
             text.slice(0, 4000),
             "fr-FR-DeniseNeural"
         );
 
-        return Buffer.from(audio);
-
-    } catch (err) {
-
-        console.error("===== EDGE TTS =====");
-        console.error(err);
+        console.log(result);
 
         return null;
-
+    } catch (err) {
+        console.error(err);
+        return null;
     }
-
 }
 
 module.exports = {
