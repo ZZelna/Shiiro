405013df3f406a1369d73e43dc80c16f44281322
ZZelna <eric.canisius1010101010@gmail.com>
2026-07-10 15:18:09 +0200

    Update tts.js

--- systems/tts.js
+++ systems/tts.js
@@ -6,13 +6,22 @@ const client = new ElevenLabsClient({
 
 async function generateVoice(text, voiceId) {
 
-    const audio = await client.textToSpeech.convert(voiceId, {
-        text,
-        model_id: "eleven_multilingual_v2",
-        output_format: "mp3_44100_128"
-    });
+    const audio = await client.textToSpeech.convert(
+        voiceId,
+        {
+            text,
+            model_id: "eleven_multilingual_v2",
+            output_format: "mp3_44100_128"
+        }
+    );
 
-    return Buffer.from(await audio.arrayBuffer());
+    const chunks = [];
+
+    for await (const chunk of audio) {
+        chunks.push(chunk);
+    }
+
+    return Buffer.concat(chunks);
 }
 
 module.exports = {
