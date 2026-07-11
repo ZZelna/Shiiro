8569fde118e32ba9ff0b18e386a8c63d1c18d86a
ZZelna <eric.canisius1010101010@gmail.com>
2026-07-12 00:17:07 +0200

    Update remove.js

--- commands/tickets/remove.js
+++ commands/tickets/remove.js
@@ -1,1 +1,27 @@
-[]
+const { TICKET_CATEGORIES } = require("../../handlers/systems/ticketHandler");
+
+module.exports = {
+    name: "remove",
+    async run(message, args) {
+
+        const isTicket = Object.values(TICKET_CATEGORIES).some(
+            cat => cat.categoryId === message.channel.parentId
+        );
+
+        if (!isTicket) {
+            return message.reply("❌ Cette commande ne fonctionne que dans un salon ticket.");
+        }
+
+        const member = message.mentions.members.first();
+
+        if (!member) {
+            return message.reply("❌ Utilisation : `+remove @membre`");
+        }
+
+        await message.channel.permissionOverwrites.delete(member.id);
+
+        return message.channel.send({
+            content: `✅ ${member} a été retiré du ticket.`
+        });
+    }
+};
