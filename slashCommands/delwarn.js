56de759b8db26f78bc521be0590ba81cab548f6e
ZZelna <eric.canisius1010101010@gmail.com>
2026-07-14 21:27:59 +0200

    Update delwarn.js

--- slashCommands/delwarn.js
+++ slashCommands/delwarn.js
@@ -1,7 +1,11 @@
 const {
-SlashCommandBuilder,
-EmbedBuilder
+    SlashCommandBuilder,
+    ContainerBuilder,
+    TextDisplayBuilder,
+    MessageFlags
 } = require("discord.js");
+
+const Warn = require("../models/Warn");
 
 const WARN1_ROLE = "1518890473727463485";
 const WARN2_ROLE = "1518890512172453898";
@@ -9,26 +13,26 @@ const WARN3_ROLE = "1518890554652496013";
 const BAN_ROLE = "1518890629310971944";
 
 const STAFF_ROLES = [
-"1506674274826584284",
-"1506678694352261301",
-"1506678765982318743",
-"1506696551706267688",
-"1507082580414173234",
-"1506696757642530982",
-"1509601528242110525",
-"1506709088451690708"
+    "1506674274826584284",
+    "1506678694352261301",
+    "1506678765982318743",
+    "1506696551706267688",
+    "1507082580414173234",
+    "1506696757642530982",
+    "1509601528242110525",
+    "1506709088451690708"
 ];
 
 module.exports = {
-data: new SlashCommandBuilder()
-.setName("delwarn")
-.setDescription("Retire un niveau de warn à un membre")
-.addUserOption(option =>
-option
-.setName("membre")
-.setDescription("Membre concerné")
-.setRequired(true)
-),
+    data: new SlashCommandBuilder()
+        .setName("delwarn")
+        .setDescription("Retire un niveau de warn à un membre")
+        .addUserOption(option =>
+            option
+                .setName("membre")
+                .setDescription("Membre concerné")
+                .setRequired(true)
+        ),
   async execute(interaction) {
 
     const member =
@@ -60,12 +64,22 @@ option
     await member.roles.remove(BAN_ROLE);
     await member.roles.add(WARN3_ROLE);
 
+    await Warn.findOneAndDelete(
+        { guildId: interaction.guild.id, userId: member.id, level: 4 },
+        { sort: { createdAt: -1 } }
+    );
+
     result = "BAN ➜ Warn 3";
 
 } else if (member.roles.cache.has(WARN3_ROLE)) {
 
     await member.roles.remove(WARN3_ROLE);
     await member.roles.add(WARN2_ROLE);
+
+    await Warn.findOneAndDelete(
+        { guildId: interaction.guild.id, userId: member.id, level: 3 },
+        { sort: { createdAt: -1 } }
+    );
 
     result = "Warn 3 ➜ Warn 2";
 
@@ -74,11 +88,21 @@ option
     await member.roles.remove(WARN2_ROLE);
     await member.roles.add(WARN1_ROLE);
 
+    await Warn.findOneAndDelete(
+        { guildId: interaction.guild.id, userId: member.id, level: 2 },
+        { sort: { createdAt: -1 } }
+    );
+
     result = "Warn 2 ➜ Warn 1";
 
 } else if (member.roles.cache.has(WARN1_ROLE)) {
 
     await member.roles.remove(WARN1_ROLE);
+
+    await Warn.findOneAndDelete(
+        { guildId: interaction.guild.id, userId: member.id, level: 1 },
+        { sort: { createdAt: -1 } }
+    );
 
     result = "Warn 1 supprimé";
 
@@ -90,20 +114,21 @@ option
     });
 
 }
-    const embed =
-        new EmbedBuilder()
-            .setColor("Green")
-            .setTitle("✅ Warn retiré")
-            .setDescription(`${member}`)
-            .addFields({
-                name: "Résultat",
-                value: result
-            })
-            .setTimestamp();
+
+    const container = new ContainerBuilder()
+        .setAccentColor(0x2ECC71)
+        .addTextDisplayComponents(
+            new TextDisplayBuilder().setContent("## ✅ Warn retiré")
+        )
+        .addTextDisplayComponents(
+            new TextDisplayBuilder().setContent(
+                `${member}\n\n**Résultat**\n${result}`
+            )
+        );
 
     await interaction.reply({
-        embeds: [embed]
+        components: [container],
+        flags: MessageFlags.IsComponentsV2
     });
 }
   };
-  
