e2365c5423d48b44df45842259fd2907fbf289fb
ZZelna <eric.canisius1010101010@gmail.com>
2026-07-15 21:42:17 +0200

    Update wrapText.js

--- handlers/systems/quote/wrapText.js
+++ handlers/systems/quote/wrapText.js
@@ -1,1 +1,39 @@
-[]
+function wrapText(ctx, text, maxWidth) {
+
+    const words = text.split(" ");
+
+    const lines = [];
+
+    let current = "";
+
+    for (const word of words) {
+
+        const test = current
+            ? `${current} ${word}`
+            : word;
+
+        if (
+            ctx.measureText(test).width > maxWidth &&
+            current
+        ) {
+
+            lines.push(current);
+
+            current = word;
+
+        } else {
+
+            current = test;
+
+        }
+
+    }
+
+    if (current)
+        lines.push(current);
+
+    return lines;
+
+}
+
+module.exports = wrapText;
