a45f27ee90fef4f824b81129de7cf40cf8e0cf1e
ZZelna <eric.canisius1010101010@gmail.com>
2026-07-15 20:24:51 +0200

    Update themes.js

--- handlers/systems/quote/themes.js
+++ handlers/systems/quote/themes.js
@@ -1,1 +1,27 @@
-[]
+module.exports = {
+
+    noir: {
+        colors: ["#000000", "#111111"],
+        text: "#FFFFFF",
+        sub: "#AAAAAA"
+    },
+
+    shiiro: {
+        colors: ["#0F172A", "#1E3A8A"],
+        text: "#FFFFFF",
+        sub: "#D4D4D8"
+    },
+
+    galaxy: {
+        colors: ["#240046", "#5A189A"],
+        text: "#FFFFFF",
+        sub: "#D8B4FE"
+    },
+
+    sunset: {
+        colors: ["#FF7E5F", "#3B1E54"],
+        text: "#FFFFFF",
+        sub: "#FFE4D6"
+    }
+
+};
