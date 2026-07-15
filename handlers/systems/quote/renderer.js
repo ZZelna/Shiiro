c84b5623d45462467f3a38f266b57b6a13d08594
ZZelna <eric.canisius1010101010@gmail.com>
2026-07-15 20:19:03 +0200

    Update renderer.js

--- handlers/systems/quote/renderer.js
+++ handlers/systems/quote/renderer.js
@@ -2,9 +2,14 @@ const { createCanvas, loadImage } = require("@napi-rs/canvas");
 
 class QuoteRenderer {
 
-    constructor() {
-        this.width = 1200;
-        this.height = 630;
+    constructor(options = {}) {
+
+        this.width = options.width || 1200;
+        this.height = options.height || 630;
+
+        this.avatarWidth = 420;
+        this.padding = 50;
+
     }
 
     async render(data) {
@@ -12,7 +17,37 @@ class QuoteRenderer {
         const canvas = createCanvas(this.width, this.height);
         const ctx = canvas.getContext("2d");
 
+        await this.drawBackground(ctx, data);
+
+        await this.drawAvatar(ctx, data);
+
+        await this.drawQuote(ctx, data);
+
+        await this.drawAuthor(ctx, data);
+
+        await this.drawWatermark(ctx);
+
         return canvas.toBuffer("image/png");
+
+    }
+
+    async drawBackground(ctx, data) {
+
+    }
+
+    async drawAvatar(ctx, data) {
+
+    }
+
+    async drawQuote(ctx, data) {
+
+    }
+
+    async drawAuthor(ctx, data) {
+
+    }
+
+    async drawWatermark(ctx) {
 
     }
 
