e182962000477f650c2cf5998468012c4957baef
ZZelna <eric.canisius1010101010@gmail.com>
2026-07-22 23:11:26 +0200

    Update QuizProfile.js

--- models/QuizProfile.js
+++ models/QuizProfile.js
@@ -1,112 +1,18 @@
-const mongoose = require("mongoose");
+const { Schema, model } = require('mongoose');
 
-const QuizProfileSchema = new mongoose.Schema({
-
-    userId: {
-        type: String,
-        required: true,
-        unique: true
-    },
-
-    level: {
-        type: Number,
-        default: 1
-    },
-
-    xp: {
-        type: Number,
-        default: 0
-    },
-
-    correct: {
-        type: Number,
-        default: 0
-    },
-
-    wrong: {
-        type: Number,
-        default: 0
-    },
-
-    totalGames: {
-        type: Number,
-        default: 0
-    },
-
-    currentStreak: {
-        type: Number,
-        default: 0
-    },
-
-    bestStreak: {
-        type: Number,
-        default: 0
-    },
-
-    fastestAnswer: {
-        type: Number,
-        default: 0
-    },
-
-    coinsWon: {
-        type: Number,
-        default: 0
-    },
-
-    categories: {
-        culture: {
-            type: Number,
-            default: 0
-        },
-        histoire: {
-            type: Number,
-            default: 0
-        },
-        geographie: {
-            type: Number,
-            default: 0
-        },
-        sciences: {
-            type: Number,
-            default: 0
-        },
-        informatique: {
-            type: Number,
-            default: 0
-        },
-        anime: {
-            type: Number,
-            default: 0
-        },
-        cinema: {
-            type: Number,
-            default: 0
-        },
-        musique: {
-            type: Number,
-            default: 0
-        },
-        sport: {
-            type: Number,
-            default: 0
-        }
-    },
-
-    achievements: {
-        type: [String],
-        default: []
-    },
-
-    lastDaily: {
-        type: Date,
-        default: null
-    }
-
-}, {
-    timestamps: true
+const quizProfileSchema = new Schema({
+  userId: { type: String, required: true },
+  guildId: { type: String, required: true },
+  xp: { type: Number, default: 0 },
+  level: { type: Number, default: 1 },
+  streak: { type: Number, default: 0 },
+  bestStreak: { type: Number, default: 0 },
+  questionsAnswered: { type: Number, default: 0 },
+  questionsCorrect: { type: Number, default: 0 },
+  lastPlayedAt: { type: Date, default: null },
 });
 
-module.exports = mongoose.model(
-    "QuizProfile",
-    QuizProfileSchema
-);
+// Un profil par joueur et par serveur
+quizProfileSchema.index({ userId: 1, guildId: 1 }, { unique: true });
+
+module.exports = model('QuizProfile', quizProfileSchema);
