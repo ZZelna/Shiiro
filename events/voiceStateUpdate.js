012b5f09ced7eb18372bda13957b3a3c2aa8d79e
ZZelna <eric.canisius1010101010@gmail.com>
2026-07-24 00:31:31 +0200

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,3 +1,25 @@
+const { createVoice } = require("../systems/voiceManager");
+const config = require("../config/voiceConfig");
+
 module.exports = async (oldState, newState) => {
 
-};
+    // Si l'utilisateur quitte un serveur
+    if (!newState.member) return;
+
+    // Il n'a rejoint aucun salon
+    if (!newState.channel) return;
+
+    // Ce n'est pas le salon "Créer un vocal"
+    if (newState.channel.id !== config.createChannelId) return;
+
+    try {
+
+        await createVoice(newState.member);
+
+    } catch (err) {
+
+        console.error("[VOICE CREATE]", err);
+
+    }
+
+}
