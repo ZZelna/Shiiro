b90c3bc80fe3d46c0ec4d3a6a0e052ab9f40e1a3
ZZelna <eric.canisius1010101010@gmail.com>
2026-07-24 00:30:36 +0200

    Update voiceManager.js

--- systems/voiceManager.js
+++ systems/voiceManager.js
@@ -1,1 +1,55 @@
-module.exports = {};
+const {
+    ChannelType,
+    PermissionFlagsBits
+} = require("discord.js");
+
+const VoiceChannel = require("../models/VoiceChannel");
+const config = require("../config/voiceConfig");
+
+async function createVoice(member) {
+
+    const guild = member.guild;
+
+    const channel = await guild.channels.create({
+
+        name: `${config.channelPrefix}${member.displayName}`,
+
+        type: ChannelType.GuildVoice,
+
+        parent: config.categoryId,
+
+        permissionOverwrites: [
+
+            {
+                id: guild.id,
+                allow: [
+                    PermissionFlagsBits.Connect,
+                    PermissionFlagsBits.ViewChannel
+                ]
+            }
+
+        ]
+
+    });
+
+    await member.voice.setChannel(channel);
+
+    await VoiceChannel.create({
+
+        guildId: guild.id,
+
+        channelId: channel.id,
+
+        ownerId: member.id
+
+    });
+
+    return channel;
+
+}
+
+module.exports = {
+
+    createVoice
+
+};
