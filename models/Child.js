689dd3ac392d038aa1f56f4593601448fc033e71
ZZelna <eric.canisius1010101010@gmail.com>
2026-07-25 10:30:09 +0900

    Update Child.js

--- models/Child.js
+++ models/Child.js
@@ -3,20 +3,33 @@ const mongoose = require("mongoose");
 module.exports = mongoose.model(
     "Child",
     new mongoose.Schema({
+
+        guildId: {
+            type: String,
+            required: true
+        },
+
         familyId: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "Family",
             required: true
         },
 
-        name: {
+        userId: {
             type: String,
             required: true
         },
 
+        name: {
+            type: String,
+            required: true,
+            maxlength: 20
+        },
+
         gender: {
             type: String,
-            enum: ["boy", "girl"]
+            enum: ["Garçon", "Fille"],
+            required: true
         },
 
         age: {
@@ -25,11 +38,6 @@ module.exports = mongoose.model(
         },
 
         happiness: {
-            type: Number,
-            default: 100
-        },
-
-        hunger: {
             type: Number,
             default: 100
         },
@@ -44,14 +52,25 @@ module.exports = mongoose.model(
             default: 0
         },
 
-        affection: {
+        school: {
             type: Number,
-            default: 100
+            default: 1
+        },
+
+        level: {
+            type: Number,
+            default: 1
+        },
+
+        xp: {
+            type: Number,
+            default: 0
         },
 
         createdAt: {
             type: Date,
             default: Date.now
         }
+
     })
 );
