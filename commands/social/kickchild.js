9e6d4dff2548980ea70844289214447fb51a7019
ZZelna <eric.canisius1010101010@gmail.com>
2026-07-25 11:47:10 +0900

    Update kickchild.js

--- commands/social/kickchild.js
+++ commands/social/kickchild.js
@@ -1,5 +1,6 @@
-const Family = require("../../models/Family");
 const Marriage = require("../../models/Marriage");
+const Family = require("../../models/Family");
+const Child = require("../../models/Child");
 
 module.exports = {
 
@@ -10,7 +11,9 @@ module.exports = {
         const target = message.mentions.members.first();
 
         if (!target)
-            return message.reply("❌ Mentionnez un enfant.");
+            return message.reply(
+                "❌ Mentionnez un enfant."
+            );
 
         const marriage = await Marriage.findOne({
             guildId: message.guild.id,
@@ -18,21 +21,32 @@ module.exports = {
         });
 
         if (!marriage)
-            return message.reply("❌ Vous devez être marié.");
+            return message.reply(
+                "❌ Vous devez être marié."
+            );
 
         const family = await Family.findOne({
             marriageId: marriage._id
         });
 
         if (!family)
-            return message.reply("❌ Famille introuvable.");
+            return message.reply(
+                "❌ Famille introuvable."
+            );
 
-        if (!family.children.includes(target.id))
-            return message.reply("❌ Cette personne n'appartient pas à votre famille.");
+        const child = await Child.findOne({
+            familyId: family._id,
+            userId: target.id
+        });
 
-        family.children = family.children.filter(id => id !== target.id);
+        if (!child)
+            return message.reply(
+                "❌ Cette personne n'appartient pas à votre famille."
+            );
 
-        await family.save();
+        await Child.deleteOne({
+            _id: child._id
+        });
 
         return message.reply(
             `👋 ${target} a été retiré de la famille **${family.name}**.`
