618f39fd59205410a81f775f23c6feb48ea60daf
ZZelna <eric.canisius1010101010@gmail.com>
2026-07-25 09:16:30 +0900

    Create family.js

---
+++ commands/social/family.js
@@ -0,0 +1,82 @@
+const {
+    EmbedBuilder
+} = require("discord.js");
+
+const Marriage = require("../../models/Marriage");
+const Family = require("../../models/Family");
+
+module.exports = {
+
+    name: "family",
+
+    async run(message) {
+
+        const marriage = await Marriage.findOne({
+
+            guildId: message.guild.id,
+
+            users: message.author.id
+
+        });
+
+        if (!marriage)
+            return message.reply("❌ Vous n'êtes pas marié.");
+
+        const family = await Family.findOne({
+
+            marriageId: marriage._id
+
+        });
+
+        if (!family)
+            return message.reply("❌ Vous n'avez pas encore créé votre famille.");
+
+        const embed = new EmbedBuilder()
+
+            .setColor("#4CAF50")
+
+            .setTitle(`🏡 ${family.name}`)
+
+            .addFields(
+
+                {
+                    name: "👑 Parents",
+                    value: family.parents.map(id => `<@${id}>`).join("\n")
+                },
+
+                {
+                    name: "👶 Enfants",
+                    value: family.children.length
+                        ? family.children.map(id => `<@${id}>`).join("\n")
+                        : "Aucun"
+                },
+
+                {
+                    name: "⭐ Niveau",
+                    value: family.level.toString(),
+                    inline: true
+                },
+
+                {
+                    name: "✨ XP",
+                    value: family.xp.toLocaleString("fr-FR"),
+                    inline: true
+                },
+
+                {
+                    name: "💰 Banque",
+                    value: `${family.coins.toLocaleString("fr-FR")} ¥`,
+                    inline: true
+                }
+
+            )
+
+            .setTimestamp();
+
+        message.reply({
+            embeds: [embed]
+        });
+
+    }
+
+};
