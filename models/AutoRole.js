d9eb54dce6cf2db9504a19ec38dc2f1569d3731d
ZZelna <eric.canisius1010101010@gmail.com>
2026-08-30 22:47:16 +0200

    Delete models directory

--- models/AutoRole.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = mongoose.model(
-    "AutoRole",
-    new mongoose.Schema({
-        guildId: String,
-        roleId: String
-    })
-);
--- models/GameChannel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = mongoose.model(
-    "GameChannel",
-    new mongoose.Schema({
-        guildId: { type: String, required: true, unique: true },
-        channelIds: { type: [String], default: [] }
-    })
-);
